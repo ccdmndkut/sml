95dbd37974315c42ae358c5c6453191a4e223907
ccdmndkut <ccdmndkut@gmail.com>
2021-03-06 20:54:03 -0500

    s

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ chars }}
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import chars from "./chars";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      chars,
+    };
+  },
+  components: {},
+};
 </script>
 
 <style lang="scss">
