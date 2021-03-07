96c2935cc3a58d9440e62a9111092640e0766603
Chris <ccdmndkut@gmail.com>
2021-03-07 02:07:10 -0500

    x

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,72 @@
 <template>
   <div>
-    <char v-for="(char, ind) in chars" :key="ind" :char="char" />
+    <!-- <char v-for="(char, ind) in chars" :key="ind" :char="char" :chars="chars" /> -->
+    {{ question + 1 }} / {{ chars.length }}
+    <div>{{ right }} / {{ wrong }}</div>
+    <char
+      v-if="chars.length && !gameover"
+      @setanswer="setanswer"
+      :key="curQ.name"
+      :char="curQ"
+      :chars="chars"
+    />
   </div>
 </template>
 
 <script>
-import chars from "./chars";
+import charsImp from "./charsb";
+
 import char from "./components/char";
 export default {
   name: "App",
   components: { char },
-  computed: {},
   data() {
     return {
-      chars,
+      chars: [],
+      question: 0,
+      right: 0,
+      wrong: 0,
     };
+  },
+  mounted() {
+    this.chars = this.shuffle(charsImp);
+  },
+  methods: {
+    setanswer(a) {
+      if (a == this.chars[this.question].name) this.right++;
+      else this.wrong++;
+      this.question++;
+    },
+    shuffle(array) {
+      return array.sort(() => Math.random() - 0.5);
+    },
+  },
+  computed: {
+    gameover() {
+      return this.question == this.chars.length;
+    },
+    curQ() {
+      return this.chars[this.question];
+    },
   },
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+@import "../node_modules/nes.css/css/nes.css";
+html,
+body,
+pre,
+code,
+kbd,
+samp,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Press Start 2P", cursive;
+}
+
+#names {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
 }
 </style>
--- src/components/char.vue
+++ src/components/char.vue
@@ -1,17 +1,91 @@
 <template>
-  <!-- <img
-    src="https://static.wikia.nocookie.net/sml/images/9/92/FullBodyMarioRemastered.png"
-    alt=""
-    srcset=""
-  /> -->
-  <div>{{ char }}</div>
+  <div id="charCont">
+    <div id="imgCont">
+      <img :src="char.url" />
+    </div>
+
+    <div id="nameCont">
+      <div @click="setAnswer" v-for="name in names" :key="name">
+        <button type="button" :class="`nes-btn is-${randBtn()}`">
+          {{ name }}
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["char"],
+  props: ["char", "chars"],
+  data() {
+    return {
+      btns: ["primary", "success", "warning"],
+    };
+  },
+  methods: {
+    randBtn() {
+      return this.btns[Math.floor(Math.random() * 2)];
+    },
+    setAnswer(ev) {
+      this.$emit("setanswer", ev.target.innerText);
+    },
+    shuffle(array) {
+      return array.sort(() => Math.random() - 0.5);
+    },
+  },
+  computed: {
+    corAnswer() {
+      return this.char.name;
+    },
+    allNames() {
+      return this.chars.map((char) => char.name);
+    },
+    names() {
+      let sample = this.shuffle(this.allNames.slice(0, 8)).filter(
+        (n) => n != this.corAnswer
+      );
+      let choices = [this.char.name, ...this.shuffle(sample)];
+      let shuffled = this.shuffle(choices);
+      return shuffled;
+    },
+  },
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+#charCont {
+  display: grid;
+  justify-items: center;
+  grid-auto-rows: 1fr auto;
+}
+#imgCont {
+  cursor: default;
+  padding: 20px;
+  width: 100vw;
+}
+img {
+  height: 300px;
+}
+#nameCont {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 10px;
+  background: rgba(209, 209, 209, 0.336);
+  padding: 30px;
+  justify-items: center;
+}
+#charCont {
+  text-align: center;
+}
+img {
+  margin-bottom: 20px;
+}
+.answer {
+  font-weight: bold;
+}
+.nes-btn {
+  overflow: hidden;
+  width: 45vw;
+  max-width: 300px;
+}
 </style>
